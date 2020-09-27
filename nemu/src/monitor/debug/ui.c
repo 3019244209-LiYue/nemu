5adea24042907d9ffa9d77db6913c329e8ac7008
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 08:19:52 +0000

    >  compile NEMU 3019244209 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:19:52 up 2 days,  1:20,  1 user,  load average: 0.24, 0.18, 0.13 1a8831133d829bf594b6705942ea2d8963aa0d97

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,19 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args) {
+        char *arg = strtok(NULL," ");
+        int step;
+        if(arg == NULL)
+               /*no argument given */
+               step = 1;
+        else
+               step = atoi(arg);
+        
+        cpu_exec(step);
+        return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +59,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+        { "si [N]", "Executes N instructions in a single step", cmd_si },
 	/* TODO: Add more commands */
 
 };
