f6fd0ecc5ff3cc44788b685af352a89375289468
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 05:59:24 +0000

    >  compile NEMU 3019244209 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:59:24 up 8 days, 23:00,  1 user,  load average: 0.08, 0.11, 0.04 f50c5823d4628c1830e62330586dd37d37af03ef

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -220,7 +220,7 @@ uint32_t expr(char *e, bool *success) {
 	/* TODO: Insert codes to evaluate the expression. */
 
 	else {
-		int val = eval(0,nr_token);
+		int val = eval(0,nr_token-1);
 		return val;
 	}	
 	panic("please implement me");
