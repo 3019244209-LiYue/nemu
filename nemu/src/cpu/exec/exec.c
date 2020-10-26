3b4542a7e51410415e8d08286281135f70dd85c7
tracer-ics2016 <tracer@njuics.org>
2020-10-26 15:35:55 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:35:55 up 13 days,  1:04,  1 user,  load average: 0.22, 0.16, 0.18 aa831f551282e8a38566ce4a1e91eca5b7dba14b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -24,13 +24,13 @@ make_group(group1_b,
 
 /* 0x81 */
 make_group(group1_v,
-	inv, inv, inv, inv, 
-	inv, inv, inv, cmp_i2rm_v)
+	add_i2rm_v, or_i2rm_v, adc_i2rm_v, sbb_i2rm_v, 
+	and_i2rm_v, sub_i2rm_v, xor_i2rm_v, cmp_i2rm_v)
 
 /* 0x83 */
 make_group(group1_sx_v,
-	add_si2rm_v, or_si2rm_v, adc_si2rm_v, inv, 
-	and_i2rm_v, inv, xor_si2rm_v, cmp_si2rm_v)
+	add_si2rm_v, or_si2rm_v, adc_si2rm_v, sbb_si2rm_v, 
+	and_i2rm_v, sub_si2rm_v, xor_si2rm_v, cmp_si2rm_v)
 
 /* 0xc0 */
 make_group(group2_i_b,                                                                                                                                                                                     
@@ -100,10 +100,10 @@ helper_fun opcode_table [256] = {
 /* 0x0c */	inv, inv, inv, _2byte_esc,
 /* 0x10 */	inv, adc_r2rm_v, inv, inv,
 /* 0x14 */	inv, inv, inv, inv,
-/* 0x18 */	inv, inv, inv, inv,
-/* 0x1c */	inv, inv, inv, inv,
-/* 0x20 */	inv, inv, inv, inv,
-/* 0x24 */	inv, inv, inv, inv,
+/* 0x18 */	sbb_r2rm_b, sbb_r2rm_v, sbb_rm2r_b, sbb_rm2r_v,
+/* 0x1c */	sbb_i2a_b, sbb_i2a_v, inv, inv,
+/* 0x20 */	add_r2rm_b, add_r2rm_v, add_rm2r_b, add_rm2r_v,
+/* 0x24 */	add_i2a_b, add_i2a_v, inv, inv,
 /* 0x28 */	sub_r2rm_b, sub_r2rm_v, sub_rm2r_b, sub_rm2r_v,
 /* 0x2c */	sub_i2a_b, sub_i2a_v, inv, inv,
 /* 0x30 */	inv, inv, inv, inv,
