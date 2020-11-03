e40098ad676db3ba7e2d22c3cbda93dfc19f4a69
tracer-ics2016 <tracer@njuics.org>
2020-11-03 15:38:28 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:38:28 up 4 days,  3:33,  1 user,  load average: 0.12, 0.16, 0.17 5dbca7a1a615410cc466ba2636f54533eb8b1184

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -183,7 +183,7 @@ static uint32_t eval(int s, int e, bool *success) {
 	}
 	else if(s == e) {
 		// single token
-		uint32_t val;
+		uint32_t val = 0;
 		switch(tokens[s].type) {
 			case REG: val = get_reg_val(tokens[s].str + 1, success);	// +1 to skip '$'
 					  if(!*success) { return 0; }
