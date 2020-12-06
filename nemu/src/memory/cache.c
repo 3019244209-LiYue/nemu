9f9b5e6e2452f0fbf7a827d3bdf4faf0b2a79f48
tracer-ics2016 <tracer@njuics.org>
2020-12-06 13:55:31 +0000

    >  compile NEMU 3019244209 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:55:31 up 8 days,  4:11,  1 user,  load average: 0.81, 0.46, 0.28 1c937eb9a652e3dddea857757c22d7f8a0f2dd18

---
+++ nemu/src/memory/cache.c
@@ -0,0 +1,75 @@
+#include "common.h"
+#include "memory/cache.h"
+#include "burst.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void ddr3_read_replace(hwaddr_t addr,void *data);
+void dram_write(hwaddr_t addr,size_t len,uint32_t data);
+void ddr3_write_replace(hwaddr_t addr,void *data,uint8_t *mask);
+
+void init_cache(){
+	int i;
+	for(i=0;i<Cache_L1_Size/Cache_L1_Block_Size;i++){
+		cache1[i].valid=0;
+	}
+	for(i=0;i<Cache_L2_Size/Cache_L2_Block_Size;i++){
+		cache2[i].valid=0;
+		cache2[i].dirty=0;
+	}
+}
+
+int read_cache1(hwaddr_t addr){
+	uint32_t group_idx=(addr>>Cache_L1_Block_Bit)&(Cache_L1_Group_Size-1);
+	uint32_t tag=(addr>>(Cache_L1_Group_Bit+Cache_L1_Block_Bit));
+
+	int i,group=group_idx*Cache_L1_Way_Size;
+	for(i=group+0;i<group+Cache_L1_Way_Size;i++){
+		if(cache1[i].valid==1&&cache1[i].tag==tag){
+			return i;
+		}
+	}
+	int pl=read_cache2(addr);
+	srand(time(0));
+	i=group+rand()%Cache_L1_Way_Size;
+	memcpy(cache1[i].data,cache2[pl].data,Cache_L1_Block_Size);
+
+	cache1[i].valid=1;
+	cache1[i].tag=tag;
+	return i;
+}
+
+int read_cache2(hwaddr_t addr){
+	uint32_t group_idx=(addr>>Cache_L2_Block_Bit)&(Cache_L2_Group_Size-1);
+	uint32_t tag=(addr>>(Cache_L2_Group_Bit+Cache_L2_Block_Bit));
+	uint32_t block_start=(addr>>Cache_L2_Block_Bit)<<Cache_L2_Block_Bit;
+
+	int i,group=group_idx*Cache_L2_Way_Size;
+	for(i=group+0;i<group+Cache_L2_Way_Size;i++){
+		if(cache2[i].valid==1&&cache2[i].tag==tag){
+			return i;
+		}
+	}
+	srand(time(0));
+	i=group+rand()%Cache_L2_Way_Size;
+	
+	if(cache2[i].valid==1&&cache2[i].dirty==1){
+		uint8_t ret[BURST_LEN<<1];
+		uint32_t block_st=(cache2[i].tag<<(Cache_L2_Group_Bit+Cache_L2_Block_Bit))|(group_idx<<Cache_L2_Block_Bit);
+		int w;
+		memset(ret,1,sizeof(ret));
+		for(w=0;w<Cache_L2_Block_Size/BURST_LEN;w++){
+			ddr3_write_replace(block_st+BURST_LEN*w,cache2[i].data+BURST_LEN*w,ret);
+		}
+	}
+	
+	int j;
+	for(j=0;j<Cache_L2_Block_Size/BURST_LEN;j++){
+		ddr3_read_replace(block_start+BURST_LEN*j,cache2[i].data+BURST_LEN*j);
+	}
+	cache2[i].dirty=0;
+	cache2[i].valid=1;
+	cache2[i].tag=tag;
+	return i;
+}
