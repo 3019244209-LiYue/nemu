70daf4b7a3f99e36e1fa62506ed58c11765d350d
tracer-ics2016 <tracer@njuics.org>
2020-12-15 13:45:50 +0000

    >  compile NEMU 3019244209 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:45:51 up 51 min,  1 user,  load average: 0.41, 0.30, 0.18 cc638e2ce732eb50de7e31ebffad87b4b97c8c0a

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,9 +3,12 @@
 #define instr movs
 
 make_helper(concat(movs_, SUFFIX)) {
-	MEM_W(cpu.edi, MEM_R(cpu.esi));
-	cpu.esi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
-	cpu.edi += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+	current_sreg = R_DS;
+	uint32_t tmp = MEM_R(reg_l(R_ESI));
+	current_sreg = R_ES;
+	swaddr_write(reg_l(R_EDI),DATA_BYTE,tmp);
+	reg_l(R_ESI) += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
+	reg_l(R_EDI) += (cpu.eflags.DF ? -DATA_BYTE : DATA_BYTE);
 
 	print_asm("movs" str(SUFFIX) " %%ds:(%%esi),%%es:(%%edi)");
 	return 1;
